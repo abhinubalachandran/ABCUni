fb9b1f564364389616ca5f132e1ad34ce80921dc
Abhinu <88822938+abhinubalachandran@users.noreply.github.com>
2025-03-15 07:45:51 +0530

    Add files via upload

---
+++ Controllers/RegistrationsController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using ABCUniversity.Models.Db;
+using Rotativa;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Xml.Linq;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.tool.xml;
+using System.Data.Entity.Migrations;
+
+namespace ABCUniversity.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        private UniversityEntities db = new UniversityEntities();
+
+        // GET: Registrations
+        public ActionResult Index()
+        {
+            return View(db.Registrations.ToList());
+        }
+        public ActionResult ConvertToPDF()
+        {
+            var printpdf = new ActionAsPdf("Index");
+            return printpdf;
+        }
+        /*public ActionResult GetSamples()
+        {
+            
+
+        }*/
+        //public ActionResult GeneratePDF()
+        //{
+        //    return new Rotativa.ActionAsPdf("GeneratePDF");
+        //}
+        //[HttpPost]
+        //[ValidateInput(false)]
+        //public FileResult Export(string GridHtml)
+        //{
+        //    using (MemoryStream stream = new System.IO.MemoryStream())
+        //    {
+        //        StringReader sr = new StringReader(GridHtml);
+        //        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+        //        PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+        //        pdfDoc.Open();
+        //        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+        //        pdfDoc.Close();
+        //        return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+        //    }
+        //}
+
+        // GET: Registrations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+        public ActionResult SendEmail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("abhinubalachandran@gmail.com", "abhinu");
+                    var receiverEmail = new MailAddress(receiver, "Receiver");
+                    var password = "abhinujackson123";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+        // GET: Registrations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RegistrationID,CandidateFirstName,CandidateLastName,DOB,Age,Phone,Email,Qualification,PerMarks,CourseName,pic,ImageFile,RowCreatedDate,Gender")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(registration.ImageFile.FileName);
+                string extension = Path.GetExtension(registration.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                registration.pic = "~/Image/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                registration.ImageFile.SaveAs(fileName);
+
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registrations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RegistrationID,CandidateFirstName,CandidateLastName,DOB,Age,Phone,Email,Qualification,PerMarks,CourseName,pic,ImageFile,RowCreatedDate,Gender")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(registration.ImageFile.FileName);
+                string extension = Path.GetExtension(registration.ImageFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                registration.pic = "~/Image/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+                registration.ImageFile.SaveAs(fileName);
+
+                db.Entry(registration).State = EntityState.Modified;
+                //db.Set<Registration>().AddOrUpdate(registration);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registration);
+        }
+        
+
+        // GET: Registrations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Registration registration = db.Registrations.Find(id);
+            if (registration == null)
+            {
+                return HttpNotFound();
+            }
+            return View(registration);
+        }
+
+        // POST: Registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Registration registration = db.Registrations.Find(id);
+            db.Registrations.Remove(registration);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
